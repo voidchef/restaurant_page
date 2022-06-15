58c3267333fee498593b6ee6301e62ff1d70321a
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-15 12:25:50 +0000

    add footer contact details

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,12 +20,28 @@ const initFooter = () => {
   siteDetails.textContent = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita repudiandae neque
   illum aspernatur fugiat maiores id magni, modi, quaerat vitae. Consectetur`;
 
+  const footerBoxTwo = document.createElement("div");
+  footerBoxTwo.classList.add("footerBoxTwo");
+
+  const footerContact = document.createElement("div");
+  footerContact.classList.add("footerContact");
+  footerContact.textContent = "Contact";
+
+  const contactDetails = document.createElement("p");
+  contactDetails.innerText = `Call: +76 (094) 754 43 7I
+  \nTiming: Everyday from 10am - 11pm
+  \nAddress: 817 N California Ave Chicago, IL 60622`;
+
   content.appendChild(footer);
   footer.appendChild(footerItems);
 
   footerItems.appendChild(footerBoxOne);
   footerBoxOne.appendChild(footerSiteName);
   footerBoxOne.appendChild(siteDetails);
+
+  footerItems.appendChild(footerBoxTwo);
+  footerBoxTwo.appendChild(footerContact);
+  footerBoxTwo.appendChild(contactDetails);
 };
 
 export { initFooter };
