a17aee64a9efd08601b0cd8ec30b9c5bf10e5c79
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-15 13:10:17 +0000

    finish project

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ bg.classList.add("bg");
 bg.setAttribute("id", "bg");
 const content = document.getElementById("content");
 content.appendChild(bg);
+
+import { initHome } from "./pages/home.js";
+initHome();
+
+import { initFooter } from "./components/footer.js";
+initFooter();
