0cc304b997b0d3d45027cfb7b5990126e72d700d
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-15 12:42:27 +0000

    add restaurant menu

---
+++ src/imageUrls.js
@@ -0,0 +1,19 @@
+import homeMenuItemImg1 from "./img/homeMenuItem1.jpg";
+import homeMenuItemImg2 from "./img/homeMenuItem2.jpg";
+import homeMenuItemImg3 from "./img/homeMenuItem3.jpg";
+import homeMenuItemImg4 from "./img/homeMenuItem4.jpg";
+import homeMenuItemImg5 from "./img/homeMenuItem5.jpg";
+import homeMenuItemImg6 from "./img/homeMenuItem6.jpg";
+import homeMenuItemImg7 from "./img/homeMenuItem7.jpg";
+import homeMenuItemImg8 from "./img/homeMenuItem8.jpg";
+
+export const homeMenuItemImgs = [
+  homeMenuItemImg1,
+  homeMenuItemImg2,
+  homeMenuItemImg3,
+  homeMenuItemImg4,
+  homeMenuItemImg5,
+  homeMenuItemImg6,
+  homeMenuItemImg7,
+  homeMenuItemImg8,
+];
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -4,28 +4,71 @@ import { homeMenuItemImgs } from "../imageUrls";
 const initMenu = () => {
   const content = document.getElementById("content");
 
-  const bg = document.querySelector(".bg");
-  bg.style.marginTop = "-50vh";
+  const bg = document.querySelector(".bg");  
+  bg.style.marginTop = "-50vh"
 
   const menuPageHeading = document.createElement("div");
   menuPageHeading.classList.add("menuPageHeading");
   menuPageHeading.textContent = "Choose according to your taste";
 
-  const menuSection = document.createElement("div");
-  menuSection.classList.add("menuSection");
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
 
-  const menuSectionHeader = document.createElement("div");
-  menuSectionHeader.classList.add("menuSectionHeader");
+  const menuHeader = document.createElement("div");
+  menuHeader.classList.add("menuHeader");
 
-  const menuSectionTitle = document.createElement("div");
-  menuSectionTitle.classList.add("menuSectionTitle");
-  menuSectionTitle.textContent = "Our Menu";
+  const menuTitle = document.createElement("div");
+  menuTitle.classList.add("menuTitle");
+  menuTitle.textContent = "Our Menu";
 
-  const menuSectionParagraph = document.createElement("div");
-  menuSectionParagraph.classList.add("menuSectionParagraph");
-  menuSectionParagraph.innerText = `Fish is one of the most wholesome foods that man can eat. In fact, people have been \n eating fish throughout human`;
+  const menuParagraph = document.createElement("div");
+  menuParagraph.classList.add("menuParagraph");
+  menuParagraph.innerText = `Fish is one of the most wholesome foods that man can eat. In fact, people have been \n eating fish throughout human`;
 
   const hr = document.createElement("hr");
+
+  const menuItemsWrapper = document.createElement("div");
+  menuItemsWrapper.classList.add("menuItemsWrapper");
+
+  const menuItems = document.createElement("div");
+  menuItems.classList.add("menuItems");
+
+  for (let index = 0; index < 8; index++) {
+    const menuCell = document.createElement("div");
+    menuCell.classList.add("menuCell");
+
+    const pizzaImg = document.createElement("img");
+    pizzaImg.src = homeMenuItemImgs[index];
+    pizzaImg.classList.add("pizzaImg");
+
+    const namePrice = document.createElement("div");
+    namePrice.classList.add("namePrice");
+
+    const pizzaName = document.createElement("div");
+    pizzaName.classList.add("pizzaName");
+    pizzaName.textContent = "Pizza margherita";
+
+    const pizzaPrice = document.createElement("div");
+    pizzaPrice.classList.add("pizzaPrice");
+    pizzaPrice.textContent = "€10";
+
+    const pizzaDescription = document.createElement("div");
+    pizzaDescription.classList.add("pizzaDescription");
+    pizzaDescription.textContent =
+      "Aeneas needs the pain of the mass. With your friends";
+
+    const orderNowBtn = document.createElement("button");
+    orderNowBtn.classList.add("orderNowBtn");
+    orderNowBtn.textContent = "Order Now";
+
+    menuItems.appendChild(menuCell);
+    menuCell.appendChild(pizzaImg);
+    menuCell.appendChild(namePrice);
+    namePrice.appendChild(pizzaName);
+    namePrice.appendChild(pizzaPrice);
+    menuCell.appendChild(pizzaDescription);
+    menuCell.appendChild(orderNowBtn);
+  }
 
   content.appendChild(bg);
   content.appendChild(menuPageHeading);
@@ -35,6 +78,8 @@ const initMenu = () => {
   menuHeader.appendChild(menuTitle);
   menuHeader.appendChild(menuParagraph);
   menuHeader.appendChild(hr);
+  menu.appendChild(menuItemsWrapper);
+  menuItemsWrapper.appendChild(menuItems);
 };
 
 export { initMenu };
