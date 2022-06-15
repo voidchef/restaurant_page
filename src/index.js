7dff45cd4c9e9958b6b52b67069f35dc165eb76a
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-15 12:14:41 +0000

    add site background image

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import "./css/index.css";
 
 import { initHeader } from "./components/header.js";
 initHeader();
+
+const bg = document.createElement("div");
+bg.classList.add("bg");
+bg.setAttribute("id", "bg");
+const content = document.getElementById("content");
+content.appendChild(bg);
