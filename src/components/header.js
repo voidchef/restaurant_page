8f26052ab0b0cfd9e1059eac073e1a9da636fcd5
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-15 12:12:21 +0000

    add site navbar

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,26 @@ const initHeader = () => {
   siteName.classList.add("siteName");
   siteName.textContent = "Site Name";
 
+  const menuTabs = document.createElement("div");
+  menuTabs.classList.add("menuTabs");
+
+  const homeTab = document.createElement("button");
+  homeTab.classList.add("homeTab", "tabs");
+  homeTab.dataset.tab = "home";
+  homeTab.textContent = "Home";
+  const menuTab = document.createElement("button");
+  menuTab.classList.add("menuTab", "tabs");
+  menuTab.dataset.tab = "menu";
+  menuTab.textContent = "Menu";
+  const aboutTab = document.createElement("button");
+  aboutTab.classList.add("aboutTab", "tabs");
+  aboutTab.dataset.tab = "about";
+  aboutTab.textContent = "About";
+  const contactTab = document.createElement("button");
+  contactTab.classList.add("contactTab", "tabs");
+  contactTab.dataset.tab = "contact";
+  contactTab.textContent = "Contact";
+
   content.appendChild(header);
 
   header.appendChild(siteName);
