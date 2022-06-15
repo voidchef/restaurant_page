033cf5fd3e3e446d81dc02d2eef6fa5527b45e44
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-15 12:24:44 +0000

    add footer site deatils

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,8 +9,23 @@ const initFooter = () => {
   const footerItems = document.createElement("div");
   footerItems.classList.add("footerItems");
 
+  const footerBoxOne = document.createElement("div");
+  footerBoxOne.classList.add("footerBoxOne");
+
+  const footerSiteName = document.createElement("div");
+  footerSiteName.classList.add("footerSiteName");
+  footerSiteName.textContent = "Site Name";
+
+  const siteDetails = document.createElement("p");
+  siteDetails.textContent = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita repudiandae neque
+  illum aspernatur fugiat maiores id magni, modi, quaerat vitae. Consectetur`;
+
   content.appendChild(footer);
   footer.appendChild(footerItems);
+
+  footerItems.appendChild(footerBoxOne);
+  footerBoxOne.appendChild(footerSiteName);
+  footerBoxOne.appendChild(siteDetails);
 };
 
 export { initFooter };
