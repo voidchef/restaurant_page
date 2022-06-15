b2f0c30239ffc5549cd6fc175afe0eddc479f589
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-15 12:47:11 +0000

    add food images

--- src/imageUrls.js
+++ src/imageUrls.js
@@ -1,3 +1,7 @@
+import homeAboutImg1 from "./img/pexels-daria-shevtsova-1260968.jpg";
+import homeAboutImg2 from "./img/pexels-christina-voinova-7368032.jpg";
+import homeAboutImg3 from "./img/pexels-brett-jordan-825661.jpg";
+
 import homeMenuItemImg1 from "./img/homeMenuItem1.jpg";
 import homeMenuItemImg2 from "./img/homeMenuItem2.jpg";
 import homeMenuItemImg3 from "./img/homeMenuItem3.jpg";
@@ -7,6 +11,7 @@ import homeMenuItemImg6 from "./img/homeMenuItem6.jpg";
 import homeMenuItemImg7 from "./img/homeMenuItem7.jpg";
 import homeMenuItemImg8 from "./img/homeMenuItem8.jpg";
 
+export const headerImages = [homeAboutImg1, homeAboutImg2, homeAboutImg3];
 export const homeMenuItemImgs = [
   homeMenuItemImg1,
   homeMenuItemImg2,
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,45 @@
 import "../css/pages/about.css";
+import {
+  headerImages,
+} from "../imageUrls";
 
 const initAbout = () => {
   const content = document.getElementById("content");
 
   const bg = document.querySelector(".bg");
-  bg.style.marginTop = "-50vh";
+  bg.style.marginTop = "-50vh"
 
   const aboutPageHeading = document.createElement("div");
   aboutPageHeading.classList.add("aboutPageHeading");
   aboutPageHeading.textContent = "We understand pizzas";
 
+  const aboutSectionWrapper = document.createElement("div");
+  aboutSectionWrapper.classList.add("aboutSectionWrapper");
+
+  const aboutSectionOne = document.createElement("div");
+  aboutSectionOne.classList.add("aboutSectionOne");
+
+  const aboutFoodImages = document.createElement("div");
+  aboutFoodImages.classList.add("aboutFoodImages");
+  const imgOne = document.createElement("img");
+  imgOne.classList.add("imgOne");
+  imgOne.src = headerImages[0];
+  const imgTwo = document.createElement("img");
+  imgTwo.classList.add("imgTwo");
+  imgTwo.src = headerImages[1];
+  const imgThree = document.createElement("img");
+  imgThree.classList.add("imgThree");
+  imgThree.src = headerImages[2];
+
   content.appendChild(bg);
   content.appendChild(aboutPageHeading);
+  content.appendChild(aboutSectionWrapper);
+  aboutSectionWrapper.appendChild(aboutSectionOne);
+
+  aboutSectionOne.appendChild(aboutFoodImages);
+  aboutFoodImages.appendChild(imgOne);
+  aboutFoodImages.appendChild(imgTwo);
+  aboutFoodImages.appendChild(imgThree);
 };
 
 export { initAbout };
